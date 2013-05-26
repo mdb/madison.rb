bad018f88abf73155571e955ae4ca19086d3a37f
Mike Ball <mikedball@gmail.com>
2013-05-26 08:20:19 -0400

    Refactored Madison as a module rather than a class

--- lib/madison.rb
+++ lib/madison.rb
@@ -1,36 +1,52 @@
 require 'json'
 
-class Madison
-  attr_accessor :states
+module Madison
+  class Map
+    attr_accessor :states
 
-  def states 
-    madison_dir = File.expand_path(File.dirname(File.dirname(__FILE__)))
-    @states ||= JSON.parse( File.open(File.join(madison_dir, "lib", "states.json")).read )
-  end
-
-  def get_abbrev(name)
-    raise ArgumentError("Argument must be a string") unless name.is_a? String
-    state_abbrevs[name.downcase]
-  end
-
-  def get_name(abbrev)
-    raise ArgumentError("Argument must be a string") unless abbrev.is_a? String
-    state_names[abbrev.downcase]
-  end
-
-  private
-  def state_names
-    @state_names ||= build_map('abbr', 'name')
-  end
-
-  def state_abbrevs
-    @state_abbrevs ||= build_map('name', 'abbr')
-  end
-
-  def build_map(in_key, out_key)
-    states.inject({}) do |map, state|
-      map[state[in_key].downcase] = state[out_key]
-      map 
+    def states 
+      madison_dir = File.expand_path(File.dirname(File.dirname(__FILE__)))
+      @states ||= JSON.parse( File.open(File.join(madison_dir, "lib", "states.json")).read )
     end
+
+    def get_abbrev(name)
+      raise ArgumentError("Argument must be a string") unless name.is_a? String
+      state_abbrevs[name.downcase]
+    end
+
+    def get_name(abbrev)
+      raise ArgumentError("Argument must be a string") unless abbrev.is_a? String
+      state_names[abbrev.downcase]
+    end
+
+    private
+    def state_names
+      @state_names ||= build_map('abbr', 'name')
+    end
+
+    def state_abbrevs
+      @state_abbrevs ||= build_map('name', 'abbr')
+    end
+
+    def build_map(in_key, out_key)
+      states.inject({}) do |map, state|
+        map[state[in_key].downcase] = state[out_key]
+        map 
+      end
+    end
+  end
+
+  @map = Map.new
+
+  def self.states
+    @map.states
+  end
+
+  def self.get_abbrev(name)
+    @map.get_abbrev(name)
+  end
+
+  def self.get_name(abbrev)
+    @map.get_name(abbrev)
   end
 end
--- spec/lib/madison_spec.rb
+++ spec/lib/madison_spec.rb
@@ -1,64 +1,136 @@
 require "spec_helper"
 
 describe Madison do
+  it "exists as a module" do
+    Madison.class.should eq Module 
+  end
+
   describe "#states" do
     it "returns a Ruby hash of state names and abbreviations" do 
-      m = Madison.new
+      m = Madison
+
       m.states[0]['name'].should === "Alabama"
       m.states[50]['abbr'].should === "WY"
       m.states.length.should === 51 
     end
-  end
-  
-  describe "#get_abbrev" do
-    before(:each) do
-      @m = Madison.new
-    end
 
-    it "returns a state's abbreviation when it's passed a state's name" do 
-      @m.get_abbrev("virginia").should === "VA"
-      @m.get_abbrev("west virginia").should === "WV"
+    describe "#get_abbrev" do
+      before(:each) do
+        @m = Madison
+      end
+
+      it "returns a state's abbreviation when it's passed a state's name" do 
+        @m.get_abbrev("virginia").should === "VA"
+        @m.get_abbrev("west virginia").should === "WV"
+      end
+      
+      it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
+        @m.get_abbrev("virginiA").should === "VA"
+        @m.get_abbrev("AlaBama").should === "AL"
+      end
+
+      it "returns nil in the event that is not passed a valid state name" do
+        @m.get_abbrev("Fake State").should === nil
+      end
+
+      it "raises an ArgumentError if it is not passed a string" do
+        lambda { @m.get_abbrev(3) }.should raise_error
+        lambda { @m.get_abbrev(nil) }.should raise_error
+        lambda { @m.get_abbrev({}) }.should raise_error
+      end
     end
     
-    it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
-      @m.get_abbrev("virginiA").should === "VA"
-      @m.get_abbrev("AlaBama").should === "AL"
-    end
+    describe "#get_name" do
+      before(:each) do
+        @m = Madison
+      end
 
-    it "returns nil in the event that is not passed a valid state name" do
-      @m.get_abbrev("Fake State").should === nil
-    end
+      it "returns a state's abbreviation when it's passed a state's name" do 
+        @m.get_name("va").should === "Virginia"
+        @m.get_name("WV").should === "West Virginia"
+      end
+      
+      it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
+        @m.get_name("vA").should === "Virginia"
+        @m.get_name("Al").should === "Alabama"
+      end
 
-    it "raises an ArgumentError if it is not passed a string" do
-      lambda { @m.get_abbrev(3) }.should raise_error
-      lambda { @m.get_abbrev(nil) }.should raise_error
-      lambda { @m.get_abbrev({}) }.should raise_error
+      it "returns nil in the event that is not passed a valid state name" do
+        @m.get_name("FS").should === nil
+      end
+
+      it "raises an ArgumentError if it is not passed a string" do
+        lambda { @m.get_name(3) }.should raise_error
+        lambda { @m.get_name(nil) }.should raise_error
+        lambda { @m.get_name({}) }.should raise_error
+      end
     end
   end
-  
-  describe "#get_name" do
-    before(:each) do
-      @m = Madison.new
+
+  describe "Madison::Map" do
+    it "exists as a class within the Madison module" do
+      Madison::Map.class.should eq Class 
     end
 
-    it "returns a state's abbreviation when it's passed a state's name" do 
-      @m.get_name("va").should === "Virginia"
-      @m.get_name("WV").should === "West Virginia"
+    describe "#states" do
+      it "returns a Ruby hash of state names and abbreviations" do 
+        m = Madison::Map.new
+        m.states[0]['name'].should === "Alabama"
+        m.states[50]['abbr'].should === "WY"
+        m.states.length.should === 51 
+      end
     end
     
-    it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
-      @m.get_name("vA").should === "Virginia"
-      @m.get_name("Al").should === "Alabama"
-    end
+    describe "#get_abbrev" do
+      before(:each) do
+        @m = Madison::Map.new
+      end
 
-    it "returns nil in the event that is not passed a valid state name" do
-      @m.get_name("FS").should === nil
-    end
+      it "returns a state's abbreviation when it's passed a state's name" do 
+        @m.get_abbrev("virginia").should === "VA"
+        @m.get_abbrev("west virginia").should === "WV"
+      end
+      
+      it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
+        @m.get_abbrev("virginiA").should === "VA"
+        @m.get_abbrev("AlaBama").should === "AL"
+      end
 
-    it "raises an ArgumentError if it is not passed a string" do
-      lambda { @m.get_name(3) }.should raise_error
-      lambda { @m.get_name(nil) }.should raise_error
-      lambda { @m.get_name({}) }.should raise_error
+      it "returns nil in the event that is not passed a valid state name" do
+        @m.get_abbrev("Fake State").should === nil
+      end
+
+      it "raises an ArgumentError if it is not passed a string" do
+        lambda { @m.get_abbrev(3) }.should raise_error
+        lambda { @m.get_abbrev(nil) }.should raise_error
+        lambda { @m.get_abbrev({}) }.should raise_error
+      end
+    end
+    
+    describe "#get_name" do
+      before(:each) do
+        @m = Madison::Map.new
+      end
+
+      it "returns a state's abbreviation when it's passed a state's name" do 
+        @m.get_name("va").should === "Virginia"
+        @m.get_name("WV").should === "West Virginia"
+      end
+      
+      it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
+        @m.get_name("vA").should === "Virginia"
+        @m.get_name("Al").should === "Alabama"
+      end
+
+      it "returns nil in the event that is not passed a valid state name" do
+        @m.get_name("FS").should === nil
+      end
+
+      it "raises an ArgumentError if it is not passed a string" do
+        lambda { @m.get_name(3) }.should raise_error
+        lambda { @m.get_name(nil) }.should raise_error
+        lambda { @m.get_name({}) }.should raise_error
+      end
     end
   end
 end
