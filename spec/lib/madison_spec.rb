3f895d00904e8a44ad7bdc8afe33e9eadf7c8ebb
Mike Ball <mikedball@gmail.com>
2014-08-28 07:35:15 -0400

    cleaned up RSpec syntax

--- spec/lib/madison_spec.rb
+++ spec/lib/madison_spec.rb
@@ -9,9 +9,9 @@ describe Madison do
     it "returns a Ruby hash of state names and abbreviations" do
       m = Madison
 
-      expect(m.states[0]['name']).to be === "Alabama"
-      expect(m.states[50]['abbr']).to be === "WY"
-      expect(m.states.length).to be === 51
+      expect(m.states[0]['name']).to eq "Alabama"
+      expect(m.states[50]['abbr']).to eq "WY"
+      expect(m.states.length).to eq 51
     end
 
     describe "#get_abbrev" do
@@ -20,17 +20,17 @@ describe Madison do
       end
 
       it "returns a state's abbreviation when it's passed a state's name" do 
-        expect(@m.get_abbrev("virginia")).to be === "VA"
-        expect(@m.get_abbrev("west virginia")).to be === "WV"
+        expect(@m.get_abbrev("virginia")).to eq "VA"
+        expect(@m.get_abbrev("west virginia")).to eq "WV"
       end
 
       it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
-        expect(@m.get_abbrev("virginiA")).to be === "VA"
-        expect(@m.get_abbrev("AlaBama")).to be === "AL"
+        expect(@m.get_abbrev("virginiA")).to eq "VA"
+        expect(@m.get_abbrev("AlaBama")).to eq "AL"
       end
 
       it "returns nil in the event that is not passed a valid state name" do
-        expect(@m.get_abbrev("Fake State")).to be === nil
+        expect(@m.get_abbrev("Fake State")).to eq nil
       end
 
       it "raises an ArgumentError if it is not passed a string" do
@@ -46,17 +46,17 @@ describe Madison do
       end
 
       it "returns a state's abbreviation when it's passed a state's name" do 
-        expect(@m.get_name("va")).to be === "Virginia"
-        expect(@m.get_name("WV")).to be === "West Virginia"
+        expect(@m.get_name("va")).to eq "Virginia"
+        expect(@m.get_name("WV")).to eq "West Virginia"
       end
 
       it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
-        expect(@m.get_name("vA")).to be === "Virginia"
-        expect(@m.get_name("Al")).to be === "Alabama"
+        expect(@m.get_name("vA")).to eq "Virginia"
+        expect(@m.get_name("Al")).to eq "Alabama"
       end
 
       it "returns nil in the event that is not passed a valid state name" do
-        expect(@m.get_name("FS")).to be === nil
+        expect(@m.get_name("FS")).to eq nil
       end
 
       it "raises an ArgumentError if it is not passed a string" do
@@ -75,9 +75,9 @@ describe Madison do
     describe "#states" do
       it "returns a Ruby hash of state names and abbreviations" do
         m = Madison::Map.new
-        expect(m.states[0]['name']).to be === "Alabama"
-        expect(m.states[50]['abbr']).to be === "WY"
-        expect(m.states.length).to be === 51
+        expect(m.states[0]['name']).to eq "Alabama"
+        expect(m.states[50]['abbr']).to eq "WY"
+        expect(m.states.length).to eq 51
       end
     end
 
@@ -87,17 +87,17 @@ describe Madison do
       end
 
       it "returns a state's abbreviation when it's passed a state's name" do 
-        expect(@m.get_abbrev("virginia")).to be === "VA"
-        expect(@m.get_abbrev("west virginia")).to be === "WV"
+        expect(@m.get_abbrev("virginia")).to eq "VA"
+        expect(@m.get_abbrev("west virginia")).to eq "WV"
       end
 
       it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
-        expect(@m.get_abbrev("virginiA")).to be === "VA"
-        expect(@m.get_abbrev("AlaBama")).to be === "AL"
+        expect(@m.get_abbrev("virginiA")).to eq "VA"
+        expect(@m.get_abbrev("AlaBama")).to eq "AL"
       end
 
       it "returns nil in the event that is not passed a valid state name" do
-        expect(@m.get_abbrev("Fake State")).to be === nil
+        expect(@m.get_abbrev("Fake State")).to be_nil
       end
 
       it "raises an ArgumentError if it is not passed a string" do
@@ -113,17 +113,17 @@ describe Madison do
       end
 
       it "returns a state's abbreviation when it's passed a state's name" do 
-        expect(@m.get_name("va")).to be === "Virginia"
-        expect(@m.get_name("WV")).to be === "West Virginia"
+        expect(@m.get_name("va")).to eq "Virginia"
+        expect(@m.get_name("WV")).to eq "West Virginia"
       end
 
       it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
-        expect(@m.get_name("vA")).to be === "Virginia"
-        expect(@m.get_name("Al")).to be === "Alabama"
+        expect(@m.get_name("vA")).to eq "Virginia"
+        expect(@m.get_name("Al")).to eq "Alabama"
       end
 
       it "returns nil in the event that is not passed a valid state name" do
-        expect(@m.get_name("FS")).to be === nil
+        expect(@m.get_name("FS")).to eq nil
       end
 
       it "raises an ArgumentError if it is not passed a string" do
