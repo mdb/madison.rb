e735e4558c8f2e9d0c09225ca962a5e177e38fef
Mike Ball <mikedball@gmail.com>
2014-08-28 07:32:17 -0400

    whitespace removal

--- spec/lib/madison_spec.rb
+++ spec/lib/madison_spec.rb
@@ -2,16 +2,16 @@ require "spec_helper"
 
 describe Madison do
   it "exists as a module" do
-    expect(Madison.class).to eq Module 
+    expect(Madison.class).to eq Module
   end
 
   describe "#states" do
-    it "returns a Ruby hash of state names and abbreviations" do 
+    it "returns a Ruby hash of state names and abbreviations" do
       m = Madison
 
       expect(m.states[0]['name']).to be === "Alabama"
       expect(m.states[50]['abbr']).to be === "WY"
-      expect(m.states.length).to be === 51 
+      expect(m.states.length).to be === 51
     end
 
     describe "#get_abbrev" do
@@ -23,7 +23,7 @@ describe Madison do
         expect(@m.get_abbrev("virginia")).to be === "VA"
         expect(@m.get_abbrev("west virginia")).to be === "WV"
       end
-      
+
       it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
         expect(@m.get_abbrev("virginiA")).to be === "VA"
         expect(@m.get_abbrev("AlaBama")).to be === "AL"
@@ -39,7 +39,7 @@ describe Madison do
         expect { @m.get_abbrev({}) }.to raise_error
       end
     end
-    
+
     describe "#get_name" do
       before(:each) do
         @m = Madison
@@ -49,7 +49,7 @@ describe Madison do
         expect(@m.get_name("va")).to be === "Virginia"
         expect(@m.get_name("WV")).to be === "West Virginia"
       end
-      
+
       it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
         expect(@m.get_name("vA")).to be === "Virginia"
         expect(@m.get_name("Al")).to be === "Alabama"
@@ -69,18 +69,18 @@ describe Madison do
 
   describe "Madison::Map" do
     it "exists as a class within the Madison module" do
-      expect(Madison::Map.class).to eq Class 
+      expect(Madison::Map.class).to eq Class
     end
 
     describe "#states" do
-      it "returns a Ruby hash of state names and abbreviations" do 
+      it "returns a Ruby hash of state names and abbreviations" do
         m = Madison::Map.new
         expect(m.states[0]['name']).to be === "Alabama"
         expect(m.states[50]['abbr']).to be === "WY"
-        expect(m.states.length).to be === 51 
+        expect(m.states.length).to be === 51
       end
     end
-    
+
     describe "#get_abbrev" do
       before(:each) do
         @m = Madison::Map.new
@@ -90,7 +90,7 @@ describe Madison do
         expect(@m.get_abbrev("virginia")).to be === "VA"
         expect(@m.get_abbrev("west virginia")).to be === "WV"
       end
-      
+
       it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
         expect(@m.get_abbrev("virginiA")).to be === "VA"
         expect(@m.get_abbrev("AlaBama")).to be === "AL"
@@ -106,7 +106,7 @@ describe Madison do
         expect { @m.get_abbrev({}) }.to raise_error
       end
     end
-    
+
     describe "#get_name" do
       before(:each) do
         @m = Madison::Map.new
@@ -116,7 +116,7 @@ describe Madison do
         expect(@m.get_name("va")).to be === "Virginia"
         expect(@m.get_name("WV")).to be === "West Virginia"
       end
-      
+
       it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
         expect(@m.get_name("vA")).to be === "Virginia"
         expect(@m.get_name("Al")).to be === "Alabama"
