2457589f2b2341b393516d8f357f483ff273e48b
Mike Ball <mikedball@gmail.com>
2014-08-29 09:10:30 -0400

    spec cleanup

--- spec/lib/madison_spec.rb
+++ spec/lib/madison_spec.rb
@@ -1,135 +1,167 @@
 require "spec_helper"
 
 describe Madison do
-  it "exists as a module" do
+
+  it 'exists as a module' do
     expect(Madison.class).to eq Module
   end
 
-  describe "#states" do
-    it "returns a Ruby hash of state names and abbreviations" do
-      m = Madison
-
-      expect(m.states[0]['name']).to eq "Alabama"
-      expect(m.states[50]['abbr']).to eq "WY"
-      expect(m.states.length).to eq 51
+  describe '#states' do
+    it 'returns an array of hashes representing each US state' do
+      expect(Madison.states.length).to eq 51
     end
 
-    describe "#get_abbrev" do
-      before(:each) do
-        @m = Madison
+    context 'each hash in the array' do
+      it 'contains a "name" key, the value of which is a state name' do
+        expect(Madison.states[0]['name']).to eq "Alabama"
       end
 
-      it "returns a state's abbreviation when it's passed a state's name" do 
-        expect(@m.get_abbrev("virginia")).to eq "VA"
-        expect(@m.get_abbrev("west virginia")).to eq "WV"
+      it 'contains an "abbr" key, the value of which is a state abbreviation' do
+        expect(Madison.states[50]['abbr']).to eq "WY"
+      end
+    end
+
+    describe '#get_abbrev' do
+      it "returns a state's abbreviation when it's passed a state's name" do
+        expect(Madison.get_abbrev("virginia")).to eq "VA"
+        expect(Madison.get_abbrev("west virginia")).to eq "WV"
       end
 
-      it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
-        expect(@m.get_abbrev("virginiA")).to eq "VA"
-        expect(@m.get_abbrev("AlaBama")).to eq "AL"
+      it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do
+        expect(Madison.get_abbrev("virginiA")).to eq "VA"
+        expect(Madison.get_abbrev("AlaBama")).to eq "AL"
       end
 
-      it "returns nil in the event that is not passed a valid state name" do
-        expect(@m.get_abbrev("Fake State")).to eq nil
+      it 'returns nil in the event that is not passed a valid state name' do
+        expect(Madison.get_abbrev('Fake State')).to eq nil
       end
 
-      it "raises an ArgumentError if it is not passed a string" do
-        expect { @m.get_abbrev(3) }.to raise_error
-        expect { @m.get_abbrev(nil) }.to raise_error
-        expect { @m.get_abbrev({}) }.to raise_error
+      context 'when it is not passed a string' do
+        it 'raises an argument error when it is passed an integer' do
+          expect { Madison.get_abbrev(3) }.to raise_error
+        end
+
+        it 'raises an argument error when it is passed nil' do
+          expect { Madison.get_abbrev(nil) }.to raise_error
+        end
+
+        it 'raises an ArgumentError if it is passed a hash' do
+          expect { Madison.get_abbrev({}) }.to raise_error
+        end
       end
     end
 
     describe "#get_name" do
-      before(:each) do
-        @m = Madison
+      it "returns a state's abbreviation when it's passed a state's name" do
+        expect(Madison.get_name("va")).to eq "Virginia"
+        expect(Madison.get_name("WV")).to eq "West Virginia"
       end
 
-      it "returns a state's abbreviation when it's passed a state's name" do 
-        expect(@m.get_name("va")).to eq "Virginia"
-        expect(@m.get_name("WV")).to eq "West Virginia"
-      end
-
-      it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
-        expect(@m.get_name("vA")).to eq "Virginia"
-        expect(@m.get_name("Al")).to eq "Alabama"
+      it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do
+        expect(Madison.get_name("vA")).to eq "Virginia"
+        expect(Madison.get_name("Al")).to eq "Alabama"
       end
 
       it "returns nil in the event that is not passed a valid state name" do
-        expect(@m.get_name("FS")).to eq nil
+        expect(Madison.get_name("FS")).to eq nil
       end
 
-      it "raises an ArgumentError if it is not passed a string" do
-        expect { @m.get_name(3) }.to raise_error
-        expect { @m.get_name(nil) }.to raise_error
-        expect { @m.get_name({}) }.to raise_error
+      context "when it is not passed a string" do
+        it "raises an ArgumentError if it is passed an integer" do
+          expect { Madison.get_name(3) }.to raise_error
+        end
+
+        it "raises an ArgumentError if it is passed nil" do
+          expect { Madison.get_name(nil) }.to raise_error
+        end
+
+        it "raises an ArgumentError if it is passed a hash" do
+          expect { Madison.get_name({}) }.to raise_error
+        end
       end
     end
   end
 
   describe "Madison::Map" do
+    let (:map) { Madison::Map.new }
+
     it "exists as a class within the Madison module" do
       expect(Madison::Map.class).to eq Class
     end
 
     describe "#states" do
-      it "returns a Ruby hash of state names and abbreviations" do
-        m = Madison::Map.new
-        expect(m.states[0]['name']).to eq "Alabama"
-        expect(m.states[50]['abbr']).to eq "WY"
-        expect(m.states.length).to eq 51
+      it "returns a an array of state names and abbreviations" do
+        expect(map.states.length).to eq 51
+      end
+
+      context "an item within the #states array" do
+        it "contains a 'name' key the value of which is the state name" do
+          expect(map.states[0]['name']).to eq "Alabama"
+        end
+
+        it "contains an 'abbr' key, the value of which is the state abbreviation" do
+          expect(map.states[50]['abbr']).to eq "WY"
+        end
       end
     end
 
     describe "#get_abbrev" do
-      before(:each) do
-        @m = Madison::Map.new
+      it "returns a state's abbreviation when it's passed a state's name" do
+        expect(map.get_abbrev("virginia")).to eq "VA"
+        expect(map.get_abbrev("west virginia")).to eq "WV"
       end
 
-      it "returns a state's abbreviation when it's passed a state's name" do 
-        expect(@m.get_abbrev("virginia")).to eq "VA"
-        expect(@m.get_abbrev("west virginia")).to eq "WV"
-      end
-
-      it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
-        expect(@m.get_abbrev("virginiA")).to eq "VA"
-        expect(@m.get_abbrev("AlaBama")).to eq "AL"
+      it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do
+        expect(map.get_abbrev("virginiA")).to eq "VA"
+        expect(map.get_abbrev("AlaBama")).to eq "AL"
       end
 
       it "returns nil in the event that is not passed a valid state name" do
-        expect(@m.get_abbrev("Fake State")).to be_nil
+        expect(map.get_abbrev("Fake State")).to be_nil
       end
 
-      it "raises an ArgumentError if it is not passed a string" do
-        expect { @m.get_abbrev(3) }.to raise_error
-        expect { @m.get_abbrev(nil) }.to raise_error
-        expect { @m.get_abbrev({}) }.to raise_error
+      context "when it is not passed a string" do
+        it "raises an ArgumentError if it is passed an integer" do
+          expect { map.get_abbrev(3) }.to raise_error
+        end
+
+        it "raises an ArgumentError if it is passed a nil" do
+          expect { map.get_abbrev(nil) }.to raise_error
+        end
+
+        it "raises an ArgumentError if it is passed a hash" do
+          expect { map.get_abbrev({}) }.to raise_error
+        end
       end
     end
 
     describe "#get_name" do
-      before(:each) do
-        @m = Madison::Map.new
+      it "returns a state's abbreviation when it's passed a state's name" do
+        expect(map.get_name("va")).to eq "Virginia"
+        expect(map.get_name("WV")).to eq "West Virginia"
       end
 
-      it "returns a state's abbreviation when it's passed a state's name" do 
-        expect(@m.get_name("va")).to eq "Virginia"
-        expect(@m.get_name("WV")).to eq "West Virginia"
-      end
-
-      it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do 
-        expect(@m.get_name("vA")).to eq "Virginia"
-        expect(@m.get_name("Al")).to eq "Alabama"
+      it "returns a state's abbreviation when it's passed a state's name, indpendent of the argument's case" do
+        expect(map.get_name("vA")).to eq "Virginia"
+        expect(map.get_name("Al")).to eq "Alabama"
       end
 
       it "returns nil in the event that is not passed a valid state name" do
-        expect(@m.get_name("FS")).to eq nil
+        expect(map.get_name("FS")).to eq nil
       end
 
-      it "raises an ArgumentError if it is not passed a string" do
-        expect { @m.get_name(3) }.to raise_error
-        expect { @m.get_name(nil) }.to raise_error
-        expect { @m.get_name({}) }.to raise_error
+      context "when it's not passed a string" do
+        it "raises an ArgumentError if it is passed an integer" do
+          expect { map.get_name(3) }.to raise_error
+        end
+
+        it "raises an ArgumentError if it is passed nil" do
+          expect { map.get_name(nil) }.to raise_error
+        end
+
+        it "raises an ArgumentError if it is passed a hash" do
+          expect { map.get_name({}) }.to raise_error
+        end
       end
     end
   end
