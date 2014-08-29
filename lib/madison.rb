f4d7aa968a89a2618f303e197c7b1bd2cba8fc96
Mike Ball <mikedball@gmail.com>
2014-08-29 11:58:05 -0400

    re-organized source code

--- lib/madison.rb
+++ lib/madison.rb
@@ -1,46 +1,8 @@
-require 'json'
+require 'madison/version'
+require 'madison/map'
 
 module Madison
-  class Map
-    attr_accessor :states
-
-    def states 
-      madison_dir = File.expand_path(File.dirname(File.dirname(__FILE__)))
-      @states ||= JSON.parse( File.open(File.join(madison_dir, "lib", "states.json")).read )
-    end
-
-    def get_abbrev(name)
-      validate_arg(name)
-      state_abbrevs[name.downcase]
-    end
-
-    def get_name(abbrev)
-      validate_arg(abbrev)
-      state_names[abbrev.downcase]
-    end
-
-    private
-    def state_names
-      @state_names ||= build_map('abbr', 'name')
-    end
-
-    def state_abbrevs
-      @state_abbrevs ||= build_map('name', 'abbr')
-    end
-
-    def build_map(in_key, out_key)
-      states.inject({}) do |map, state|
-        map[state[in_key].downcase] = state[out_key]
-        map 
-      end
-    end
-
-    def validate_arg(arg)
-      raise ArgumentError, "Argument '#{arg}' must be a string" unless arg.is_a? String
-    end
-  end
-
-  @map = Map.new
+  @map = Madison::Map.new
 
   def self.states
     @map.states
